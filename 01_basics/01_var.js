e81cdd6385c1c9752eb5d7d905f50a20453c5d19
Karishma Singhal <105881155+karishmasinghal2002@users.noreply.github.com>
2024-07-16 19:38:26 +0000

    container config done

--- 01_basics/01_var.js
+++ 01_basics/01_var.js
@@ -1,3 +1,5 @@
+// practice code (examples)
+
 const accountid = 144553
 let accountemail = "karishma@gmail.com"
 var accountpwd = "12345"
@@ -8,4 +10,35 @@ accountpwd = "020202"
 accountcity = "delhi "
 // accountid = 2
 console.log(accountid);
-console.table([accountid, accountemail, accountpwd, accountcity])
+console.table([accountid, accountemail, accountpwd, accountcity]) //to print multiple variables
+
+let score=33
+console.log(typeof score);
+
+// prefer not to use because of issue in block scope and functional scope
+
+let Score = "33" //string
+console.log(typeof Score);
+
+let valueInNumber = Number(Score)
+console.log(typeof valueInNumber); 
+console.log(valueInNumber); //"33" =>33, "33abc"=>nan, true=>1
+
+let age=null
+console.log(typeof age);
+
+let name=undefined
+console.log(typeof name);
+
+let login=1
+let boolean=Boolean(login)
+console.log(boolean); //1=>true, 0=>false
+
+let int=""
+let bol=Boolean(int)
+console.log(bol); //""=>false
+
+let some=33
+let string=String(some)
+console.log(string);
+console.log(typeof string);
