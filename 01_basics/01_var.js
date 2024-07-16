8bc1d41f50d8d730107e68c67a95cbd7d9b736c9
Karishma Singhal <105881155+karishmasinghal2002@users.noreply.github.com>
2024-07-16 18:29:40 +0000

    container config done

--- 01_basics/01_var.js
+++ 01_basics/01_var.js
@@ -3,5 +3,8 @@ let accountemail = "karishma@gmail.com"
 var accountpwd = "12345"
 accountcity = "jaipur"
 
-accountid = 2
+accountemail = "2610ks@gmai.com"
+accountpwd = "020202"
+accountcity = "delhi "
+// accountid = 2
 console.log(accountid);
